9ece67db4284b52a47874c9454d9729c4ff94330
kalecode2 <kalecode2@gmail.com>
2024-12-03 12:44:05 +0400

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -46,7 +46,8 @@ products.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary js-add-to-cart"
+      <button class="add-to-cart-button 
+      button-primary js-add-to-cart"
        data-product-id="${product.id}">
         Add to Cart
       </button>
@@ -60,26 +61,31 @@ document.querySelector('.js-products-grid')
 
 document.querySelectorAll('.js-add-to-cart')
   .forEach((button) => {
-      button.addEventListener('click', () => {
-        const productId = button.dataset.productId;
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
 
-        let matchingItem;
-        cart.forEach((item) => {
-          if (productId === item.productId) {
-            matchingItem = item;
-          }
-        });
-
-        if (matchingItem) {
-          matchingItem.quantity += 1;
-        } else {
-          cart.push({
-            productId: productId,
-            quantity: 1,
-          });
+      let matchingItem;
+      cart.forEach((item) => {
+        if (productId === item.productId) {
+          matchingItem = item;
         }
-
-        console.log(cart);
-
       });
+
+      if (matchingItem) {
+        matchingItem.quantity += 1;
+      } else {
+        cart.push({
+          productId: productId,
+          quantity: 1,
+        });
+      }
+
+      let cartQuantity = 0;
+      cart.forEach((item) => {
+        cartQuantity += item.quantity;
+      });
+      document.querySelector('.js-cart-quantity')
+        .innerHTML = cartQuantity;
+
+    });
   });
